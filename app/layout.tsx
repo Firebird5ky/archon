473c49a485068e26024ec373ce6fb4af1ab4fbb9
Firebird5ky <hoekscott1@gmail.com>
2026-04-29 19:49:25 -0400

    fix favicon in public folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,10 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'ARCHON',
   description: 'The faction network',
+  icons: {
+    icon: '/favicon.png',
+    apple: '/favicon.png',
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
